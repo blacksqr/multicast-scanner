d0b616ba5b23e65001edc338617eca135aaeb3fd
marttj <marttj@f31e3ecd-513f-0410-be1a-63948e0c647e>
2007-12-07 23:04:19 +0000

    Update API so multicastScan just takes a list.  Export the expand calls so a user can easily make the list.

--- mcastscan_example.tcl
+++ mcastscan_example.tcl
@@ -5,8 +5,9 @@ exec tclsh "$0" "$@"
 # Copyright (c) 2007 by Todd J Martin <todd.martin@acm.org>
 
 lappend auto_path [pwd]
-set ipRange [list 224.0.0.10 224.0.0.13]
-set portRange [list 8000 8005]
+package require mcastscan
+set ipRange [mcastscan::expandIpList 224.0.0.10 224.0.0.11]
+set portRange [mcastscan::expandPortList 8000 8001]
 
 package require mcastscan
 
