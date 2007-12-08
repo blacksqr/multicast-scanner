ea18923736f475dca5bc936757d11d09c0c0dee4
marttj <marttj@f31e3ecd-513f-0410-be1a-63948e0c647e>
2007-12-08 00:46:12 +0000

    Scan ports and groups in parrallel to improve speed.  This relies on my TclUdp patches.

--- trunk/mcastscan_example.tcl
+++ trunk/mcastscan_example.tcl
@@ -4,19 +4,31 @@ exec tclsh "$0" "$@"
 
 # Copyright (c) 2007 by Todd J Martin <todd.martin@acm.org>
 
+# If you run this on Linux and see lots of errors, it is likely because of
+# kernel limits for the maximum number of groups a socket can join and how much
+# memory the networking subsystem can use.  These commands (run as root)
+# increase these limits and allow this script to run:
+#   /sbin/sysctl -w net.ipv4.igmp_max_memberships=2000
+#   /sbin/sysctl -w net.core.optmem_max=102400
+
 lappend auto_path [pwd]
 package require mcastscan
-set ipRange [mcastscan::expandIpList [list 224.0.0.10 224.0.0.11]]
-set portRange [mcastscan::expandPortList [list 8000 8001]]
+set ipRange [mcastscan::expandIpList [list 224.0.0.1 224.0.7.1]]
+set portRange [mcastscan::expandPortList [list 7534 7535]]
 
 package require mcastscan
 
 proc printUpdate {key status} {
-    puts "$key = $status"
+    if {![string equal $status "timeout"]} {
+	puts "Update: $key = $status"
+    }
 }
 
 set statusList [mcastscan::multicastScan $ipRange $portRange 10 printUpdate]
 
+puts "\nFinal Results\n-------------"
 foreach {key status} $statusList {
-    puts "$key = $status"
+    if {![string equal $status "timeout"]} {
+	puts "$key = $status"
+    }
 }
